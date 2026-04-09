0ed449f0b437ab0e32aae62dbb070176f7e39ce9
Andrew Park <ayepark@icloud.com>
2026-04-09 09:30:29 -0700

    feat: implement computer and human choice logic in new script.js file linked to index.html

---
+++ script.js
@@ -0,0 +1,54 @@
+// Rock Paper Scissors
+
+// I. Writing Logic to get Computer Choice:
+// Our game will be played against the computer. We will write a function that randomly returns "rock","paper", or "scissors".
+
+// 1. Create a new function named getComputerChoice.
+function getComputerChoice() {
+  // we want to generate random numbers
+  // we want those random numbers to correspond to 3 choices: rock, paper, or scissors
+  // this generates a random integer between 0 and 3 and will return the greatest integer less than or equal to its numeric argument. (so either 0, 1, or 2)
+
+  // 2. Write the code so that getComputerChoice will randomly return one of the following string values: "rock", "paper", or "scissors".
+  let randomNumber = Math.floor(Math.random() * 3)
+  console.log(randomNumber)
+
+  // - Hint: The Math.random method returns a random number that's greater than or equal to 0 and less than 1.
+  //         Think about how we can use this to conditionally return one of the multiple choices.
+
+  // 3. Test that our function returns what we expect using console.log before advancing to next step.
+  if (randomNumber === 0) {
+    console.log("Rock")
+    return "Rock"
+  } else if (randomNumber === 1) {
+    console.log("Paper")
+    return "Paper"
+  } else {
+    console.log("Scissors")
+    return "Scissors"
+  }
+}
+
+// getComputerChoice()
+
+// Arrow function form:
+const getComputerChoiceArrow = () =>
+  ["Rock", "Paper", "Scissors"][Math.floor(Math.random() * 3)]
+
+console.log(getComputerChoiceArrow())
+
+// II. Write Logic to get Human Choice:
+// Our game will be played by a human player. We will write a function that takes a user's input and returns it.
+
+// 1. Create a new function named getHumanChoice
+function getHumanChoice() {
+  let userInput = window.prompt("Rock, Paper, or Scissors? ")
+  console.log(userInput)
+}
+
+getHumanChoice()
+
+// 2. Write the code so that getHumanChoice will return one of the valid choices depending on what the user inputs.
+// - Hint: Use prompt method to get the user's input.
+
+// 3. Test our function returns using console.log
